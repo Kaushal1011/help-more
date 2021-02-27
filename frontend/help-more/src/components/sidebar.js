108cb585cd4ed6687519b96e846dbf74e6af1bce
Kaushal1011 <kaushalpatil10@gmail.com>
2021-02-27 23:00:57 +0530

    navbar sidebar and timeout hook for later

---
+++ frontend/help-more/src/components/sidebar.js
@@ -0,0 +1,102 @@
+import React, { useEffect, useState } from "react";
+import clsx from "clsx";
+import { makeStyles } from "@material-ui/core/styles";
+import Drawer from "@material-ui/core/Drawer";
+import Button from "@material-ui/core/Button";
+import IconButton from "@material-ui/core/IconButton";
+import List from "@material-ui/core/List";
+import Divider from "@material-ui/core/Divider";
+import ListItem from "@material-ui/core/ListItem";
+import MenuIcon from "@material-ui/icons/Menu";
+import ListItemText from "@material-ui/core/ListItemText";
+
+const useStyles = makeStyles({
+    list: {
+        width: 250,
+    },
+    fullList: {
+        width: "auto",
+    },
+});
+
+export default function Sidebar() {
+    const classes = useStyles();
+    const [state, setState] = React.useState({
+        left: false,
+    });
+
+    const toggleDrawer = (open) => (event) => {
+        if (
+            event.type === "keydown" &&
+            (event.key === "Tab" || event.key === "Shift")
+        ) {
+            return;
+        }
+
+        setState({ ...state, left: open });
+    };
+    const pathArray = ["/add", "/list", "/rewards", "/admin", "/invest"];
+    const list = () => (
+        <div
+            className={clsx(classes.list)}
+            role="presentation"
+            onClick={toggleDrawer(false)}
+            onKeyDown={toggleDrawer(false)}
+        >
+            <List>
+                {["Get Help", "Help People", "Request Rewards"].map(
+                    (text, index) => (
+                        <ListItem
+                            button
+                            onClick={(e) => {
+                                toggleDrawer(false);
+                                window.location.href = pathArray[index];
+                            }}
+                            key={text}
+                        >
+                            <ListItemText primary={text} />
+                        </ListItem>
+                    )
+                )}
+            </List>
+            <Divider />
+            <List>
+                {["Admin Dashboard", "Investor Dashboard"].map(
+                    (text, index) => (
+                        <ListItem
+                            button
+                            onClick={(e) => {
+                                toggleDrawer(false);
+                                window.location.href = pathArray[index + 3];
+                            }}
+                            key={text}
+                        >
+                            <ListItemText primary={text} />
+                        </ListItem>
+                    )
+                )}
+            </List>
+        </div>
+    );
+
+    return (
+        <React.Fragment key="left">
+            <IconButton
+                edge="start"
+                className={classes.menuButton}
+                color="inherit"
+                aria-label="menu"
+                onClick={toggleDrawer(true)}
+            >
+                <MenuIcon />
+            </IconButton>
+            <Drawer
+                anchor="left"
+                open={state["left"]}
+                onClose={toggleDrawer(false)}
+            >
+                {list()}
+            </Drawer>
+        </React.Fragment>
+    );
+}
